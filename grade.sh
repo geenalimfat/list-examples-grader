bb357c7cd8be05f2b1b177360851a0aaf6f8bcca
geenalimfat <glimfat@ucsd.edu>
2022-11-10 18:54:33 -0800

    gg

--- grade.sh
+++ grade.sh
@@ -5,3 +5,22 @@ set -e
 rm -rf student-submission
 git clone $1 student-submission
 
+cd student-submission
+
+if [[ ! -e ListExamples.java ]]
+then 
+    echo "wrong test file submitted"
+    exit 1
+fi
+cd ..
+
+cp TestListExamples.java student-submission
+
+set +e
+
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
+
+
+
+echo "no"
